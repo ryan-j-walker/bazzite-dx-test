b4c4cb8745a48c9ede2a67c549f948d30e238796
Ryan Walker <46801660+ryan-j-walker@users.noreply.github.com>
2025-07-02 19:55:28 -0400

    Update vscode.sh

--- files/scripts/vscode.sh
+++ files/scripts/vscode.sh
@@ -2,10 +2,10 @@
 
 set -oue pipefail
 
-dnf5 install -y --enable-repo="docker-ce-stable" "${docker_pkgs[@]}" || {
-    # Use test packages if docker pkgs is not available for f42
-    if (($(lsb_release -sr) == 42)); then
-        echo "::info::Missing docker packages in f42, falling back to test repos..."
-        dnf5 install -y --enablerepo="docker-ce-test" "${docker_pkgs[@]}"
-    fi
-}
+dnf5 config-manager addrepo --set=baseurl="https://packages.microsoft.com/yumrepos/vscode" --id="vscode"
+dnf5 config-manager setopt vscode.enabled=0
+# FIXME: gpgcheck is broken for vscode due to it using `asc` for checking
+# seems to be broken on newer rpm security policies.
+dnf5 config-manager setopt vscode.gpgcheck=0
+dnf5 install --nogpgcheck --enable-repo="vscode" -y \
+    code
