b1e462fe4d842767623d51e9da4eeb84ff1c7a82
Ryan Walker <46801660+ryan-j-walker@users.noreply.github.com>
2025-07-02 19:46:25 -0400

    Create vscode.sh

---
+++ files/scripts/vscode.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+dnf5 install -y --enable-repo="docker-ce-stable" "${docker_pkgs[@]}" || {
+    # Use test packages if docker pkgs is not available for f42
+    if (($(lsb_release -sr) == 42)); then
+        echo "::info::Missing docker packages in f42, falling back to test repos..."
+        dnf5 install -y --enablerepo="docker-ce-test" "${docker_pkgs[@]}"
+    fi
+}
